059b8f03602b3babd034fe147ea985327086df53
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-02-24 23:30:59 +0900

    Apply Prettier

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,14 +10,14 @@ export const watch = async (req, res) => {
     // const id = req.params.id;
     const video = await Video.findById(id);
     if (!video) {
-        return res.status(404).render("404", { pageTitle: "Video not found" })
-    };
-    return res.render("watch", { pageTitle: video.title, video })
-}
+        return res.status(404).render("404", { pageTitle: "Video not found" });
+    }
+    return res.render("watch", { pageTitle: video.title, video });
+};
 
 export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload" });
-}
+};
 
 export const postUpload = async (req, res) => {
     const file = req.file;
@@ -26,7 +26,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            videoUrl : file.path,
+            videoUrl: file.path,
             hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
@@ -36,24 +36,24 @@ export const postUpload = async (req, res) => {
             errorMessage: error._message,
         });
     }
-}
+};
 
 export const getEdit = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
     if (!video) {
-        return res.status(404).ender("404", { pageTitle: "Video not found" })
-    };
+        return res.status(404).ender("404", { pageTitle: "Video not found" });
+    }
     return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
-}
+};
 
 export const postEdit = async (req, res) => {
     const { id } = req.params;
     const { title, description, hashtags } = req.body;
     const video = await Video.findById(id);
     if (!video) {
-        return res.status(404).render("404", { pageTitle: "Video not found" })
-    };
+        return res.status(404).render("404", { pageTitle: "Video not found" });
+    }
     await Video.findByIdAndUpdate(id, {
         title,
         description,
@@ -74,9 +74,9 @@ export const search = async (req, res) => {
     if (keyword) {
         videos = await Video.find({
             title: {
-                $regex: new RegExp(`${keyword}`, "i")
+                $regex: new RegExp(`${keyword}`, "i"),
             },
-        })
+        });
     }
     return res.render("search", { pageTitle: "Search", videos });
-}
+};
