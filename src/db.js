10057f810f3f4b2c558433116df414246d399257
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-03-13 12:24:43 +0900

    Update db error message

--- src/db.js
+++ src/db.js
@@ -9,5 +9,5 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
 
 const db = mongoose.connection;
 
-db.on("error", (error) => console.log("❌ DB Error", error));
+db.on("error", (error) => console.log("❌ DB Error:", error));
 db.once("open", () => console.log("✅ Connected to DB"));
