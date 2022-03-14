114d95d74a9a491e767d2d39d5a2173a3c9f14ec
Kim-Hyunseo <kimhyunseo305@gmail.com>
2022-03-15 05:38:21 +0900

    #9.2 Webpack Configuration part Two

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
+const path = require("path");
+
 module.exports = {
-    // source code
+    // entry: source code
     entry: "./src/client/js/main.js",
+    mode: "development", // <-> "production"
     output: {
         filename: "main.js",
-        path: "./statics/js",
+        // __dirname: JS에서 제공하는 폴더까지의 절대 경로
+        path: path.resolve(__dirname, "statics", "js"),
+    },
+    module: {
+        rules: [
+            {
+                // 모든 JS 파일에 적용
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["@babel/preset-env", { targets: "defaults" }]],
+                    },
+                },
+            },
+        ],
     },
 };
